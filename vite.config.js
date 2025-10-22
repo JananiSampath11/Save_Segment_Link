d1be39f4212d943636fb524df74229821a649648
unknown <jananisambath11@gmail.com>
2025-10-23 01:02:42 +0530

    push  commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'Save_Segment_Link',
+	base: '/Save_Segment_Link/',
 });
